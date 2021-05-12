8aa1529351d2f2172088d966e43c2da876ca0b1d
JosiahPhua <josiahphuajy@gmail.com>
2021-05-12 12:31:20 +0800

    changed JS to have newGame logic

--- new.js
+++ new.js
@@ -18,51 +18,16 @@ let totalScore = document.getElementById("scoreCount");
 
 let $time = document.getElementById("countdown");
 
-let time = 20;
+let overlay = document.getElementsByClassName("overlay")
+
+let time;
 
 let scoreCount = [];
 
 let overallScore =[];
 
 
-// let level = [1, 2, 3, 4, 5];
-
-// for (const i of level) {
-//     if (i == 1){
-//         time = 20;
-//         // Ending the game in 30seconds
-//         setTimeout(() => {
-//             timeUp(time);
-//         }, 30000);
-        
-//     } else if (i == 2){
-//         time = 15;
-//         // Ending the game in 25 seconds
-
-//     } else if (i == 3){
-//         time = 10;
-//         // Ending the game in 20 seconds
-
-//     } else if (i == 4){
-//         time = 5;
-//         // Ending the game in 15 seconds
-
-//     } else if (i == 5){
-//         time = 0;
-//         // Ending the game in 10 seconds
-
-//     }
-    
-// }
-
 // This is for 20s countdown.
-setTimeout(() => {
-
-
-
-
-    console.log("Hi");
-}, 20000);
 
 
 let jump = ()=>  {
@@ -77,68 +42,23 @@ let jump = ()=>  {
     }, 500)
 }
 
-window.addEventListener("keydown", event => {
-    console.log(event);
-    if (event.code === "Space"){
-        jump(eevee);
-    }
-});
+
 let point = 5;
 
-let checkPoints = setInterval(function(){
-    let eeveeTop = parseInt(window.getComputedStyle(eevee).getPropertyValue("top"));
-    let BTRight = parseInt(window.getComputedStyle(berryTime).getPropertyValue("left"));
-    let BPRight = parseInt(window.getComputedStyle(berryPoint).getPropertyValue("left"));
-    let TSRight = parseInt(window.getComputedStyle(thunderstone).getPropertyValue("left"));
-
-    if (eeveeTop <= 200 && BPRight < 200 && BPRight > 100){
-        berryPoint.classList.remove("bpMove");
-        berryPoint.style.display = "none";
-        // call function to add show total score in the div?
-        addPoint(point);   
-    }
-    if (eeveeTop <= 200 && BTRight < 200 && BTRight > 100){
-        berryTime.classList.remove("btMove");
-        berryTime.style.display = "none";
-        time+= 5;
-        // call function to add time * multiplier to score?
-    }
-    if (eeveeTop <= 200 && TSRight < 200 && TSRight > 100){
-        thunderstone.classList.remove("TS");
-        thunderstone.style.display = "none";
-        imgJolteon.src = "./images/jolteon.gif";
-        // call function to add show total score in the div?
-        addPoint(point*2);   
-    }
-
-}, 10);
-
-let generateBP = setInterval(function(){
-    berryPoint.classList.add("bpMove");
-    berryPoint.style.display = "block";
-}, 1000);
-
-function addPoint(points){
-    scoreCount.push(points);
-    let sum = scoreCount.reduce(function(a,b){
-        return a+b;
-    })
-    totalScore.innerText = sum
-}
-
-
 // Timer
-(function countDown() {
+function countDown() {
     $time.textContent = time + " second" + (time == 1 ? "" : "s")
     if (time-- > 0) setTimeout(countDown, 1000) 
 // this 2000 means 2000miliseconds, can write a function to drop the time
-})();
+};
 
 
 function gameEnd(){
     eevee.style.display = "none";
     berryPoint.style.display = "none";
     berryTime.style.display = "none";
+    totalScore.style.display = "none";
+
 }
 
 // create function to carry time over from previous stage.
@@ -159,8 +79,56 @@ function timeUp(time) {
         console.log(overallScoreInt);
     }
 }
-<<<<<<< HEAD
+
+let newGame = (level) => {
+    let checkPoints = setInterval(function(){
+        let eeveeTop = parseInt(window.getComputedStyle(eevee).getPropertyValue("top"));
+        let BTRight = parseInt(window.getComputedStyle(berryTime).getPropertyValue("left"));
+        let BPRight = parseInt(window.getComputedStyle(berryPoint).getPropertyValue("left"));
+        let TSRight = parseInt(window.getComputedStyle(thunderstone).getPropertyValue("left"));
+    
+        if (eeveeTop <= 200 && BPRight < 200 && BPRight > 100){
+            berryPoint.classList.remove("bpMove");
+            berryPoint.style.display = "none";
+            // call function to add show total score in the div?
+            addPoint(point);   
+        }
+        if (eeveeTop <= 200 && BTRight < 200 && BTRight > 100){
+            berryTime.classList.remove("btMove");
+            berryTime.style.display = "none";
+            time+= 5;
+            // call function to add time * multiplier to score?
+        }
+        if (eeveeTop <= 200 && TSRight < 200 && TSRight > 100){
+            thunderstone.classList.remove("TS");
+            thunderstone.style.display = "none";
+            imgJolteon.src = "./images/jolteon.gif";
+            // call function to add show total score in the div?
+            addPoint(point*2);   
+        }
+    
+    }, 10);
+
+    let generateBP = setInterval(function(){
+        berryPoint.classList.add("bpMove");
+        berryPoint.style.display = "block";
+    }, 1000);
+    
+    function addPoint(points){
+        scoreCount.push(points);
+        let sum = scoreCount.reduce(function(a,b){
+            return a+b;
+        })
+        totalScore.innerText = sum
+    }
+
+    window.addEventListener("keydown", event => {
+        console.log(event);
+        if (event.code === "Space"){
+            jump(eevee);
+        }
+    });
 
 
-=======
->>>>>>> 1a8c0d333247ed2ec16c2e0019c152e869b4b28f
+}
+
